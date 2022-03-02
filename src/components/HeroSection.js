bd27d97ea01cc1230111545a10bffd2d9500b7d2
Witchayada Muenprom <6410110481@email.psu.ac.th>
2022-03-02 11:41:42 +0700

    edit page layout

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>Welcome</h1>
-      <h1>To</h1>
+      <h1>to</h1>
       <h1>KPS automation</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
