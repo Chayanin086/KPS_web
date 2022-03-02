a8e9ba4166f801edfa4da0498b65a77e4610ab16
JatnipatSangkanee <6410110081@psu.ac.th>
2022-03-02 21:24:48 +0700

    contact

---
+++ src/components/Contactcard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Contactcard.css';
+import { Link } from 'react-router-dom';
+
+function Contactcard() {
+  return (
+    <div className='contact-container'>
+      <section className='contact-subscription'>
+        <h1 className='contact-subscription-heading'>
+          Contact
+        </h1>
+      </section>
+      <div class='contact-links'>
+        <div className='contact-link-wrapper'>
+        <div class='contact-logo'>
+            <Link to='/' className='contact-logo'>
+              KPS AUTOMATION
+            
+            </Link>
+          </div>
+          <div class='contact-link-items'>
+            <h2>ที่อยู่</h2>
+            <p>เค.พี.เอส. ออโตเมชั่น    90/1   ม.4 
+          </p>
+            <p>ต.ช้างขวา อ.กาญจนดิษฐ์ จ. สุราษฎร์ธานี    </p>
+            <p> 84160</p>
+
+          </div>
+          <div class='contact-link-items'>
+            <h2>ช่องทางการติดต่อ</h2>
+            <p i class='fas fa-phone-square-alt' >   phone   :      084-6648294</p>
+            <p i class= 'fa fa-envelope'>        email  :   phasitsophon1970@gmail.com​</p>
+            <p i class = "fab fa-line fa-5x" >     Line         :          084-6648294</p>
+          
+          </div>
+        </div>
+        
+          
+      
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Contactcard;
--- src/components/page/Contactus.js
+++ src/components/page/Contactus.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../../App.css';
 import Contacthead from '../Contacthead';
 import GoogleMap from '../Mapcard';
+import Contactcard from '../Contactcard'
 function Contactus() {
   return (
    <>
      <Contacthead/>
      <GoogleMap/>
+     <Contactcard/>
    </> 
   );
 
